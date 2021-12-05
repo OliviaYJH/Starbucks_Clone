7de47fa7440bb76011e5144bc28619c81eeac19f
OliviaYJH <0703olivia@naver.com>
2021-12-05 13:18:41 +0900

    add: recommand menu

--- app/src/main/java/com/example/myapplication/Home.kt
+++ app/src/main/java/com/example/myapplication/Home.kt
@@ -11,7 +11,7 @@ import com.example.myapplication.databinding.HomeBinding
 
 class Home : Fragment(){
 
-    var sentenceArray = arrayOf("찬 바람 불어오는 겨울\n감기 조심하세요", "편안한 밤 되세요", "추운 겨울엔 역시\n따뜻한 릴렉서 한 잔")
+    var sentenceArray = arrayOf("찬 바람 불어오는 겨울\n감기 조심하세요", "오늘 같은 오후엔\n스노우 민트초콜릿", "추운 겨울엔 역시\n따뜻한 릴렉서 한 잔")
 
     private lateinit var binding: HomeBinding
 
@@ -21,6 +21,12 @@ class Home : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = HomeBinding.inflate(layoutInflater)
+
+        binding.ivLatte.clipToOutline = true
+        binding.ivFrappuccino.clipToOutline = true
+        binding.ivCitron.clipToOutline = true
+        binding.ivLime.clipToOutline = true
+
         return binding.root;
     }
 
